beaab9ecbb07148d1db8362dbb50de9572993de6
zufan <Zufanllw@gmail.com>
2023-02-16 14:35:04 -0800

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle, followed by a new line."
+ * return: Always 0
+ */
+
+int main(void)
+
+{
+	puts("\"programming is like building multilingual puzzle");
+
+	return(0);
+}	
