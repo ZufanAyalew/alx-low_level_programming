053d4e981a19fff85b61bfc00869712def662874
zufan <Zufanllw@gmail.com>
2023-02-17 04:03:24 -0800

    Print alphabet

---
+++ 2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always o (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
+
