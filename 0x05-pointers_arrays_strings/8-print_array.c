a3a0b54a5720603f57ec405ca47bfc55723b2713
zufan <Zufanllw@gmail.com>
2023-02-27 07:00:14 -0800

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,7 +3,7 @@
 
 /**
  * print_array - Prints n elements of an array of integers
- * @n: Array of integers
+ * @a: Array of integers
  * @n: Number of elements of the array to be printed
  * Return: void
  */
