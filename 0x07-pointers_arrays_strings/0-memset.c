fecadb1e8526fa7e11058abb2e5c96e7776997d5
zufan <Zufanllw@gmail.com>
2023-03-06 07:15:15 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,7 @@
  * _memset - Entry point
  * @s: pointed destination
  * @b: constant byte
- * @n: bytes 
+ * @n: bytes
  * Return: Always 0 (Success)
  */
 char *_memset(char *s, char b,  unsigned int n)
